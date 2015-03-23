d8876b90006dd14d75fa9946713320e560a7baf1
Tim <tim@midokura.com>
2015-03-23 18:42:48 -0400

    Changes to reenable virtual nodes and gossip

--- cassandra-cluster/scripts/cassandra-clusternode.sh
+++ cassandra-cluster/scripts/cassandra-clusternode.sh
@@ -27,14 +27,14 @@ fi
 sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$CASSANDRA_SEEDS\"/" $CASSANDRA_CONFIG/cassandra.yaml
 
 # With virtual nodes disabled, we need to manually specify the token
-if [ -z "$CASSANDRA_TOKEN" ]; then
-	echo "Missing initial token for Cassandra"
-	exit -1
-fi
-echo "JVM_OPTS=\"\$JVM_OPTS -Dcassandra.initial_token=$CASSANDRA_TOKEN\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
+#if [ -z "$CASSANDRA_TOKEN" ]; then
+#	echo "Missing initial token for Cassandra"
+#	exit -1
+#fi
+#echo "JVM_OPTS=\"\$JVM_OPTS -Dcassandra.initial_token=$CASSANDRA_TOKEN\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
 
 # Most likely not needed
-echo "JVM_OPTS=\"\$JVM_OPTS -Djava.rmi.server.hostname=$IP\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
+#echo "JVM_OPTS=\"\$JVM_OPTS -Djava.rmi.server.hostname=$IP\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
 
 echo "Starting Cassandra on $IP..."
 
